1a4768297ddb4db4c17a9a9e93a9b921c67de12d
casept <davids.paskevics@gmail.com>
2021-07-03 16:03:31 +0200

    Abort if argument isn't supplied

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,15 @@
 #include <fmt/core.h>
 
+#include <cstdlib>
+
 #include "metainfo.hpp"
 #include "torrent.hpp"
 #include "tracker.hpp"
 
 int main(int argc, char** argv) {
     if (argc != 2) {
-        fmt::print(stderr, "Usage : {} file.torrent", argv[0]);
+        fmt::print(stderr, "Usage : {} file.torrent\n", argv[0]);
+        exit(EXIT_FAILURE);
     }
     const auto metainfo = tt::metainfo_from_path(argv[1]);
     auto torrent = tt::Torrent(metainfo);
