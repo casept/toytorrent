6949299812418a94619169c76e1302ea5f84e3bb
casept <davids.paskevics@gmail.com>
2021-08-06 17:41:53 +0200

    Add more linters

--- shell.nix
+++ shell.nix
@@ -1,9 +1,10 @@
 let
   sources = import ./nix/sources.nix;
   niv = import sources.niv { inherit sources; };
-  arion = import sources.arion { };
-  pkgs = import sources.nixpkgs { };
-in pkgs.mkShell {
+  arion = import sources.arion {};
+  pkgs = import sources.nixpkgs {};
+in
+pkgs.mkShell {
   CMAKE_INCLUDE_PATH = "${pkgs.curlFull.dev}/include";
   CMAKE_LIBRARY_PATH = "${pkgs.curlFull}/lib";
   CMAKE_PREFIX_PATH = "${pkgs.curlFull}";
@@ -19,18 +20,22 @@ in pkgs.mkShell {
     pkgs.pkgconfig
     pkgs.ninja
     pkgs.cppcheck
+    pkgs.flawfinder
     pkgs.include-what-you-use
     pkgs.python3
     pkgs.valgrind
     pkgs.gdb
-    (pkgs.callPackage ./nix/pkgs/camomilla {
-      pythonXXPackages = pkgs.python39Packages;
-    })
+    (
+      pkgs.callPackage ./nix/pkgs/camomilla {
+        pythonXXPackages = pkgs.python39Packages;
+      }
+    )
+    (pkgs.callPackage ./nix/pkgs/lizard {})
 
     # Libraries
     pkgs.botan2
     pkgs.curlFull
-    (pkgs.callPackage ./nix/pkgs/cpr { })
+    (pkgs.callPackage ./nix/pkgs/cpr {})
     pkgs.fmt
 
     # For tests
