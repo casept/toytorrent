0dab65a252698dc6db76c4683094302da14851e2
casept <davids.paskevics@gmail.com>
2022-03-12 21:31:11 +0100

    Format nix

--- flake.nix
+++ flake.nix
@@ -13,49 +13,50 @@
       in
       {
         devShell = pkgs.mkShell {
-            CMAKE_INCLUDE_PATH = "${pkgs.curlFull.dev}/include";
-  CMAKE_LIBRARY_PATH = "${pkgs.curlFull}/lib";
-  CMAKE_PREFIX_PATH = "${pkgs.curlFull}";
-  LOCALE_ARCHIVE = "${pkgs.glibcLocales}/lib/locale/locale-archive";
+          CMAKE_INCLUDE_PATH = "${pkgs.curlFull.dev}/include:${pkgs.botan2}/include/botan-2";
+          CMAKE_LIBRARY_PATH = "${pkgs.curlFull}/lib";
+          CMAKE_PREFIX_PATH = "${pkgs.curlFull}";
+          LOCALE_ARCHIVE = "${pkgs.glibcLocales}/lib/locale/locale-archive";
           buildInputs = with pkgs; [
-    # Dev tooling
-    pkgs.clang_12
-    pkgs.clang-tools
-    pkgs.ccache
-    pkgs.cmake
-    pkgs.extra-cmake-modules
-    pkgs.cmake-format
-    pkgs.pkgconfig
-    pkgs.ninja
-    pkgs.cppcheck
-    pkgs.flawfinder
-    pkgs.include-what-you-use
-    pkgs.python3
-    pkgs.valgrind
-    pkgs.gdb
-    (
-      pkgs.callPackage ./nix/pkgs/camomilla {
-        pythonXXPackages = pkgs.python39Packages;
-      }
-    )
-    (pkgs.callPackage ./nix/pkgs/lizard {})
+            # Dev tooling
+            pkgs.clang_13
+            pkgs.clang-tools
+            pkgs.ccache
+            pkgs.cmake
+            pkgs.extra-cmake-modules
+            pkgs.cmake-format
+            pkgs.pkgconfig
+            pkgs.ninja
+            pkgs.cppcheck
+            pkgs.flawfinder
+            pkgs.include-what-you-use
+            pkgs.python3
+            pkgs.valgrind
+            pkgs.gdb
+            (
+              pkgs.callPackage ./nix/pkgs/camomilla {
+                pythonXXPackages = pkgs.python39Packages;
+              }
+            )
+            (pkgs.callPackage ./nix/pkgs/lizard { })
 
-    # Libraries
-    pkgs.botan2
-    pkgs.curlFull
-    (pkgs.callPackage ./nix/pkgs/cpr {})
-    pkgs.fmt
+            # Libraries
+            pkgs.botan2
+            pkgs.curlFull
+            (pkgs.callPackage ./nix/pkgs/cpr { })
+            pkgs.fmt_8
 
-    # For tests
-    pkgs.gtest
-    pkgs.aria2
-    pkgs.opentracker
-    pkgs.boost175
+            # For tests
+            pkgs.gtest
+            pkgs.aria2
+            pkgs.opentracker
+            pkgs.boost175
 
-    # Nix support
-    pkgs.niv
-    arion
-  ];
+            # Nix support
+            pkgs.rnix-lsp
+            pkgs.nixpkgs-fmt
+            arion
+          ];
         };
       });
 }
